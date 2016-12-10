442ffb118aa155cd6a415a50eca90c8567e9a5f0
Piezoelectric <gerald.hu.99@gmail.com>
2016-12-09 20:44:47 -0600

    Added guilds menu and first-time configuration. Renamed the file to SDiscordArchiver.py, from SDA-v07-shared.py.

--- SDiscordArchiver.py
+++ SDiscordArchiver.py
@@ -5,23 +5,26 @@ import time
 import datetime #only used at the end, to indicate when a log was taken
 import codecs
 import os #used to delete the reversed log, once the complete log is generated
+import glob #used to find config.txt
 
 '''
-Dec 3 2016
-Release for Global Hack Day
-NOTE: The "Client ID" and "Client Secret" parameters have been stripped
-from this file. To run this, get your own Client ID and Client Secret from
-here:
-https://discordapp.com/developers/applications/me
-and click "New Application"
+Dec 9 2016
+ADDED:
+1) config.txt (force a user to get their own application/hiding my own tokens)
+2) Guilds menu
 
+So now SDA can get every kind of text channel!
 
-This code will NOT FUNCTION without Client ID and Client Secret
+TODO:
+1) get rid of that awful copypaste stuff, set up localhost:5000 to do it automatically
+using flask or something
+2) put a log in its own folder, and attachments in a subfolder, rather than
+barfing them all up into the same directory
 '''
 
-#MESSAGE WRITING FUNCTIONS
+#====MESSAGE WRITING FUNCTIONS====
 def writeMessages(channelName, channelID, messages):
-    #prettifies retrieved messages
+    #prettifies retrieved messages from getMessages
     filenameRev = channelName+"-"+channelID+"-reversed.txt"
     filehandleRev = codecs.open(filenameRev, 'a', 'utf-8')
     
@@ -50,6 +53,7 @@ def writeMessages(channelName, channelID, messages):
         filehandleRev.write(message)
 
     filehandleRev.flush() #puts all messages in memory into the file
+    #print("writeMessages() has finished.")
 
 def fileReverse(channelName, channelID):
     #reverses a full message log
@@ -58,7 +62,7 @@ def fileReverse(channelName, channelID):
     lines = filehandleRev.readlines()
     lines.reverse()
 
-    filename = channelName+"-"+channelID+"-LOG.txt"
+    filename = "MESSAGE-LOG-"+channelName+"-"+channelID+".txt"
     filehandle = codecs.open(filename, 'w', 'utf-8')
     filehandle.write("Log for channel '" + channelName + "', created at " + str(datetime.datetime.now()) + "\n")
     filehandle.write("Created in Simple Discord Archiver v07.\n\n")
@@ -70,9 +74,10 @@ def fileReverse(channelName, channelID):
     filehandleRev.close()
     os.remove(filenameRev)
 
-#MESSAGE RETRIEVAL FUNCTIONS
-def getDMs(localToken):
-    counter = 0
+    print("fileReverse() has finished.")
+
+#====CHANNEL RETRIEVAL FUNCTIONS====
+def getDMChannels(localToken):
     userChannelsGET = "https://discordapp.com/api/users/@me/channels?token="+localToken+"&limit=100"
     response = requests.get(userChannelsGET)
     
@@ -87,36 +92,114 @@ def getDMs(localToken):
     channel = userChannels[chanNumber]
     chanName = channel["recipient"]["username"]+"-DM"
     chanID = channel["id"]
+    return chanID, chanName
 
-    chanMessagesGET = "https://discordapp.com/api/channels/"+chanID+"/messages?token="+localToken
+def getUserGuild(localToken):
+    userGuildsGET = "https://discordapp.com/api/users/@me/guilds"
+    response = requests.get(userGuildsGET, headers=OAUTHhead)
+
+    userGuilds = json.loads(response.content.decode('utf-8'))
+    print("Found " + str(len(userGuilds)) + " servers.")
+    for i in range(len(userGuilds)):
+        guild = userGuilds[i]
+        print("["+str(i)+"]: " + userGuilds[i]["name"])
+
+    print("Enter the number of the server you want to access.")
+    guildNumber = int(input(">"))
+    guildID = userGuilds[guildNumber]["id"]
+    guildName = userGuilds[guildNumber]["name"]
+    return guildID, guildName
+
+def getChannelFromGuild(localToken, guildID, guildName):
+    guildGET = "https://discordapp.com/api/guilds/"+guildID+"/channels?token="+localToken
+    response = requests.get(guildGET)
+
+    guildChannels = json.loads(response.content.decode('utf-8'))
+    guildTextChannels = []
+    for channel in guildChannels: #filter out voice channels
+        if channel["type"] == "text":
+            guildTextChannels.append(channel)
+            
+    print("Found " + str(len(guildTextChannels)) + " text channels in this server.")
+    for i in range(len(guildTextChannels)):
+        channel = guildTextChannels[i]
+        print("["+str(i)+"]: " + guildTextChannels[i]["name"])
+
+    print("Enter the number of the channel you want to archive.")
+    chanNumber = int(input(">"))
+    channel = guildChannels[chanNumber]
+    chanName = "channel-"+channel["name"]+"-in-"+guildName
+    chanID = channel["id"]
+    return chanID, chanName
+
+#====MESSAGE RETRIEVAL FUNCTIONS====
+def getMessages(localToken, channelID, channelName):
+    counter = 0
+
+    chanMessagesGET = "https://discordapp.com/api/channels/"+channelID+"/messages?token="+localToken
     response = requests.get(chanMessagesGET)
     messages = json.loads(response.content.decode("utf-8"))
 
     while len(messages) != 0:
-        print("DEBUG: Now on iteration " + str(counter))
-        counter = counter+1
-        writeMessages(chanName, chanID, messages)
+        print("DEBUG: Retrieving messages " + str(counter+1) + " to " +str(counter+100))
+        counter = counter+100
+        writeMessages(channelName, channelID, messages) #prettifies and writes the "messages" JSON-object
         print("Ok, resting.")
         time.sleep(5)
         
         #get the next set of messages
         before = messages[len(messages)-1]['id']
-        chanMessagesGET = "https://discordapp.com/api/channels/"+chanID+"/messages?token="+localToken+"&limit=100"+"&before="+before
+        chanMessagesGET = "https://discordapp.com/api/channels/"+channelID+"/messages?token="+localToken+"&limit=100"+"&before="+before
         response = requests.get(chanMessagesGET)
         messages = json.loads(response.content.decode("utf-8"))
 
-    print("Message retrieval for " + chanName + " is done.")
-    fileReverse(chanName, chanID)
-    print("Simple Discord Archiver has finished.")
+    print("Message retrieval for " + channelName + " is done.")
+    fileReverse(channelName, channelID)
+    print("getMessages() has finished.")
 
-'''
-==========MAIN===========
-'''
+#====MAIN====
+print("Thank you for using Simple Discord Archiver.\n")
 
-#GETS OAUTH TOKEN
-print('''Thank you for using Simple Discord Archiver.
+#FIND CONFIG.TXT + Load clientID, clientSecret, localToken
+cfg = glob.glob('./config.txt')
+if len(cfg) == 0:
+    print("\nconfig.txt not found. Initializing first-time setup.")
+    configHandle = open("config.txt", 'w')
+    print('''
+Please log onto the Discord website, and go to
+https://discordapp.com/developers/applications/me
+and register a new application. The application should have a ClientID
+and a Client Secret.
+    ''')
+    clientID = input("Please enter the ClientID.\n>")
+    configHandle.write(clientID+"\n")
+    clientSecret = input("Please enter the Client Secret.\n>")
+    configHandle.write(clientSecret+"\n")
+    print('''
+To export private DMs, this program also needs your local storage token.
+Please follow these instructions to access your local token:
+1) Open Discord.
+2) Press CTRL-SHIFT-I. This will bring up the console.
+3) Type "localStorage.token" into the console and press ENTER. (This is caps sensitive.)
+This will bring up a string of characters. 
+    ''')
+    localToken = input("Enter your local token below, without any quotes:\n>")
+    configHandle.write(localToken+"\n")
+    configHandle.close()
+    print("\nFirst-time setup complete. Press ENTER to continue.")
+    input("")
+else:
+    print("Loading config.txt.")
+    configHandle = open("config.txt", 'r')
+    clientID = (configHandle.readline()).rstrip()
+    clientSecret = (configHandle.readline()).rstrip()
+    localToken = (configHandle.readline()).rstrip()
+    configHandle.close()
+    print("Config.txt loaded. Press ENTER to continue.")
+    input("")
 
-When you press ENTER, a web browser window should appear, asking you for 
+#GETS OAUTH CODE to swap for token, etc
+print('''When you press ENTER, a web browser window should appear, asking you for 
 account authorization. Please click the authorize button; the program needs
 to access your account in order to access your messages.
 
@@ -125,52 +208,39 @@ which will look like this:
 
 http://localhost:5000/#code=<CODE>
 
-The webpage will never load because I still need to work on that shit.
+The webpage will never load because I still need to work on that.
 For the time being, please copypaste the <CODE> into the command line.
 
 Press ENTER to continue.''')
 input("")
-authURL = "https://discordapp.com/oauth2/authorize?client_id=<ID>&scope=guilds+identify&uri=https://localhost:5000&response_type=code"
+authURL = "https://discordapp.com/oauth2/authorize?client_id="+clientID+"&scope=guilds+identify&uri=https://localhost:5000&response_type=code"
 webbrowser.open(authURL)
 
 print("Please enter the code:")
 code = input(">")
 
-authRequestURL = "https://discordapp.com/api/oauth2/token?client_id=<ID>&client_secret=<SECRET>&"
+authRequestURL = "https://discordapp.com/api/oauth2/token?client_id="+clientID+"&client_secret="+clientSecret+"&"
 authRequestURL = authRequestURL + "grant_type=authorization_code&code=" + code + "&redirect_uri=https://localhost:5000"
 response = requests.post(authRequestURL)
 auth = json.loads(response.content.decode("utf-8"))
 OAUTHtoken = auth["access_token"]
 OAUTHhead = {"Authorization": "Bearer " + OAUTHtoken}
-print("OAUTH2 Token obtained.\n")
-
-
-#GET LOCAL TOKEN
-print('''To export private DMs, this program needs your local storage token.
-Please follow these instructions to access your local token:
-1) Open Discord.
-2) Press CTRL-SHIFT-I. This will bring up the console.
-3) Type "localStorage.token" into the console.
-This will bring up a long string of characters. 
-''')
-print("Enter your local token below, without any quotes:")
-localToken = input(">")
-print("Local token obtained.\n")
+print("\nOAUTH2 Token obtained. Program is ready to begin retrieving messages.\n")
 
 #MAIN
-print("============Setup complete.============")
 channelType = ""
-print("Archive DMs or a guild? 1 for DMs, 2 for guilds (NOT YET IMPLEMENTED)")
+print("Archive private DMs or server messages? 1 for DMs, 2 for server messages")
 while channelType != "1" and channelType != "2":
     channelType = input(">")
     
     if channelType == "1":
-        getDMs(localToken)
+        channelID, channelName = getDMChannels(localToken)
+        getMessages(localToken, channelID, channelName)
     
     elif channelType == "2":
-        userGuildsGET = "https://discordapp.com/api/users/@me/guilds"
-        response = requests.get(userGuildsGET, headers=OAUTHhead)
-        print(response)
+        guildID, guildName = getUserGuild(localToken)
+        channelID, channelName = getChannelFromGuild(localToken, guildID, guildName)
+        getMessages(localToken, channelID, channelName)
 
     else:
-        print ("Your input was not recognized. Try again.")
+        print("Your input was not recognized. Try again.")
